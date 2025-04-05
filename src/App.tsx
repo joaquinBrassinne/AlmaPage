e309d62a9bce856e78532a181a3fa5d2987c7e55
FraNko <rasiafranko@gmail.com>
2025-04-05 16:40:52 -0300

    feat: Agrego Quienes Somos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import NavBar from "./components/NavBar/NavBar";
+import QuienesSomos from "./components/QuienesSomos/QuienesSomos";
+
 import "./index.css";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="mainContainer"></div>
+      <div className="mainContainer">
+        <QuienesSomos />
+      </div>
     </>
   );
 }
