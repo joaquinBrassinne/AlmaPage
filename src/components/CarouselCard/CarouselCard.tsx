da3b2b1fb11950064bde6693259f2f0b452d578a
Pablo-r-stack <pablor.velasco.suarez@gmail.com>
2025-04-05 20:15:25 -0300

    feat: agrego componente carrusel

---
+++ src/components/CarouselCard/CarouselCard.tsx
@@ -0,0 +1,45 @@
+import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel'
+import { Card, CardContent } from '../ui/card'
+import Autoplay from 'embla-carousel-autoplay'
+import { CarouselDots } from '../ui/CarouselDots'
+
+interface Props {
+    imgList: {
+      path: string
+    }[],
+    range?: number
+  }
+
+const CarouselCard = ({imgList, range}: Props) => {
+
+        if (!imgList || imgList.length === 0) return null
+
+        return (
+          <Carousel
+            plugins={[
+              Autoplay({
+                delay: range || 4000,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              {imgList.map(({ path }, index) => (
+                <CarouselItem key={index}>
+                  <Card className="cardTransparent">
+                    <CardContent className="carouselItems">
+                      <img
+                        src={path}
+                        alt={`slide-${index}`}
+                        className="carouselImgAspect"
+                      />
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselDots />
+          </Carousel>
+    )
+}
+
+export default CarouselCard
